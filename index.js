e2b7fde42b60f4d4a464192ef64f1cbfedc95e27
Your Name <you@example.com>
2022-11-08 12:04:42 +0600

    database user name and password set

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config()
 
@@ -9,8 +10,19 @@ const port = process.env.PORT || 5000 ;
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p2qoups.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.get('/' , (req,res) => {
-  res.send('Geniois Car Running')
+  res.send('Photo Master Running')
 })
 
 app.listen(port , () => {
