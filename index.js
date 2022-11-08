2354e2646f1568dbf90b49f1d2c194201a266d36
Your Name <you@example.com>
2022-11-08 12:20:44 +0600

    added collection in db

--- index.js
+++ index.js
@@ -11,15 +11,20 @@ app.use(cors())
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p2qoups.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
-
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+  try{
+  const serviceCollection = client.db('photoMaster').collection('services')
+
+  }
+  finally{
+
+  }
+  
+}
+run().catch(err => console.error(err))
 
 app.get('/' , (req,res) => {
   res.send('Photo Master Running')
